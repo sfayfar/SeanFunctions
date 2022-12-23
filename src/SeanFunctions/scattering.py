052efbed8a881a10a429d3a83acf775701d26077
Sean Fayfar <sfayfar@mit.edu>
2022-12-23 11:20:35 -0500

    Prepared to read files

--- src/SeanFunctions/scattering.py
+++ src/SeanFunctions/scattering.py
@@ -1,13 +1,18 @@
 import pandas as pd
 import numpy as np
+# from importlib import resources
+# import io
+import pkg_resources
 
+stream = pkg_resources.resource_stream(__name__,'Data/AtomicFormFactorConstants.csv')
+aff_DF = pd.read_csv(stream)
 
 def atomic_form_factor(atom,QList,inputConst=None):
-    if atom not in df_data['element'].values:
-        print(f'The atom \'{atom}\' is not available.\nThe available atoms are:\n','\t'.join(map(str,df_data['element'].values)))
+    if atom not in aff_DF['element'].values:
+        print(f'The atom \'{atom}\' is not available.\nThe available atoms are:\n','\t'.join(map(str,aff_DF['element'].values)))
     else:
         if inputConst is None:
-            const_values = df_data[df_data['element'] == atom].iloc[:,2:-2].values[0]
+            const_values = aff_DF[aff_DF['element'] == atom].iloc[:,2:-2].values[0]
         else:
             const_values = inputConst
         sumData = 0.0
