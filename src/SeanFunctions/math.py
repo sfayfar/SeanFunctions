01a8ce32564b006928c55c680cb0305c4ed049ac
Sean Fayfar <sfayfar@mit.edu>
2023-08-16 12:07:34 -0400

    Update math.py

--- src/SeanFunctions/math.py
+++ src/SeanFunctions/math.py
@@ -323,12 +323,12 @@ def bin_ndarray(ndarray, new_shape, operation="sum"):
 
     """
     operation = operation.lower()
-    if not operation in ["sum", "mean"]:
+    if operation not in ["sum", "mean"]:
         raise ValueError("Operation not supported.")
     if ndarray.ndim != len(new_shape):
         raise ValueError("Shape mismatch: {} -> {}".format(ndarray.shape, new_shape))
     compression_pairs = [(d, c // d) for d, c in zip(new_shape, ndarray.shape)]
-    flattened = [l for p in compression_pairs for l in p]
+    flattened = [larr for p in compression_pairs for larr in p]
     ndarray = ndarray.reshape(flattened)
     for i in range(len(new_shape)):
         op = getattr(ndarray, operation)
